b135f6a64001d30b58de1a5e9349390632542e9e
PasqualeIannace <pasquale.iannace@gmail.com>
2023-12-11 15:30:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import AppComponent from "./components/AppComponent.vue"
+
+import { store } from "./store.js" //state management
+
+export default {
+	components: {
+		AppComponent
+	},
+	data() {
+		return {
+			store
+		}
+	},
+	mounted() {
+
+	},
+	methods: {
+		doThings() {
+			console.log("App.vue does things");
+		}
+	}
+}
+</script>
+
+<template>
+	<main>
+		<AppComponent />
+	</main>
+</template>
+
+<style lang="scss">
+// importo il foglio di stile generale dell'applicazione, non-scoped
+@use './styles/general.scss';
+</style>
+
+<style scoped lang="scss">
+// importo variabili
+// @use './styles/partials/variables' as *;
+
+// ...qui eventuale SCSS di App.vue
+</style>
---
+++ src/components/AppComponent.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from "../store.js" //state management
+
+export default {
+    name: "AppComponent",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Sono un componente!</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+// importo variabili
+// @use './styles/partials/variables' as *;
+
+// ...qui eventuale SCSS di AppComponent
+</style>
